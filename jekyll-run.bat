dd48e15564c27b9b7b40da4bd63c301f0bae4161
Alex Laverty <alaverty@gmail.com>
2021-12-13 21:21:07 +1100

    Initial commit

---
+++ jekyll-run.bat
@@ -0,0 +1,1 @@
+docker run --rm --volume="C:\Users\laverty\Documents\jekyll-theme-chirpy:/srv/jekyll" --volume="C:\Users\laverty\Documents\bundle:/usr/local/bundle" -p 0.0.0.0:4000:4000 -it jekyll/jekyll jekyll server
